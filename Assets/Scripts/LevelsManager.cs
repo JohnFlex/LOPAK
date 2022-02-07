8db2ff897c094fe3ae21ed1087ccf2ba4ecca7bb
Meng Louis <menglouis15@gmail.com>
2022-02-07 10:09:31 +0100

    Polish Du jeu
    -Addition de sprite
    -Addition du shader de l'eau

--- Assets/Scripts/LevelsManager.cs
+++ Assets/Scripts/LevelsManager.cs
@@ -47,7 +47,7 @@ public class LevelsManager : MonoBehaviour
                 
                 poolOfObjects[k].SetActive(true);
                 poolOfObjects[k].transform.position = new Vector3(j,-i);
-                poolOfObjects[k].GetComponent<SpriteRenderer>().color = levelAsEncoded[k] == '0' ? Color.black : Color.red;
+                poolOfObjects[k].GetComponent<SpriteRenderer>().color = levelAsEncoded[k] == '0' ? Color.black : Color.blue;
                 if (levelAsEncoded[k] == '1')
                 {
                     poolOfObjects[k].GetComponent<ChangeColorOnTrigger>().passStatus = ChangeColorOnTrigger.PassStatus.available;
